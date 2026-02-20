dbfa0ab4a57c3c11c17e746cb6491790b763194f
Karthi_CTS <karthi.m@cognizant.com>
2026-02-20 21:22:18 +0530

    Update update-pkg.cjs

--- .idx/update-pkg.cjs
+++ .idx/update-pkg.cjs
@@ -4,16 +4,16 @@ const path = require('path');
 const packageJsonPath = path.join(process.cwd(), 'package.json');
 const packageJson = require(packageJsonPath);
 
-// Add dev dependencies with updated, compatible versions
+// Add dev dependencies with updated, compatible versions for ESLint v9
 packageJson.devDependencies = {
   ...packageJson.devDependencies,
   "eslint": "^9.5.0",
-  "eslint-plugin-qwik": "^1.7.5",
-  "@typescript-eslint/eslint-plugin": "^7.13.1",
-  "@typescript-eslint/parser": "^7.13.1",
-  "eslint-config-prettier": "^9.1.0",
+  "eslint-plugin-qwik": "^1.7.5", // Supports ESLint v9
+  "@typescript-eslint/eslint-plugin": "^8.0.0-alpha.33", // v8 alpha supports ESLint v9
+  "@typescript-eslint/parser": "^8.0.0-alpha.33", // v8 alpha supports ESLint v9
+  "typescript-eslint": "^8.0.0-alpha.33", // v8 alpha supports ESLint v9
+  "eslint-config-prettier": "^9.1.0", // Supports ESLint v9
   "prettier": "^3.3.2",
-  "typescript-eslint": "^7.13.1"
 };
 
 // Add scripts
@@ -35,4 +35,4 @@ packageJson.prettier = {
 
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 
-console.log('Successfully updated package.json with latest ESLint versions!');
+console.log('Successfully updated package.json with ESLint v9 compatible versions!');
